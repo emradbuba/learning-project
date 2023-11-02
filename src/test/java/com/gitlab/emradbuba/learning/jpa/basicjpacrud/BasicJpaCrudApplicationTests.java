5084e7a8e0adb14bed2526b342587838363900c8
Radek Em <3488909-emradbuba@users.noreply.gitlab.com>
2023-11-02 20:23:06 +0100

    Initial commit

---
+++ src/test/java/com/gitlab/emradbuba/learning/jpa/basicjpacrud/BasicJpaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitlab.emradbuba.learning.jpa.basicjpacrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicJpaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
